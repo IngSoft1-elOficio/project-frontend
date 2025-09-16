f2c946286e25065b2964d1e56268d05a4afa0fad
sabino.lucas2016 <sabino.lucas2016@gmail.com>
2025-09-16 17:51:33 +0000

    ING-7 add LoginScreen.css and basic layout

--- src/containers/LoginScreen.jsx
+++ src/containers/LoginScreen.jsx
@@ -1,7 +1,16 @@
+import React from 'react';
+import './LoginScreen.css';
+
 function LoginScreen() {
   return (
-    
-  )
+    <div className="login-container" style={{ backgroundImage: `url('project-frontend/public/main-screen-bg.jpg')` }}>
+      <div className="login-form">
+        <input type="input-text" placeholder="Ingresa tu nombre" />
+        <input type="input-date" placeholder="Fecha de nacimiento" />
+        <button class="login-button">Ingresar</button>
+      </div>
+    </div>
+  );
 }
 
-export default LoginScreen
+export default LoginScreen;
