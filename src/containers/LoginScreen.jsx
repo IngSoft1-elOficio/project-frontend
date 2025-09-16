e46d592377cce370a4484aafeb9819f037c48955
sabino.lucas2016 <sabino.lucas2016@gmail.com>
2025-09-16 18:07:40 +0000

    ING-7 bug fixing

--- src/containers/LoginScreen.jsx
+++ src/containers/LoginScreen.jsx
@@ -5,7 +5,7 @@ function LoginScreen() {
   return (
     <div className="login-container" style={{ backgroundImage: `url('/main-screen-bg.jpg')` }}>
       <div className="login-form">
-        <h1>Bienvenido al Juego</h1>
+        <
         <input type="text" placeholder="Ingresa tu nombre" />
         <input type="date" placeholder="Fecha de nacimiento" />
         <button className="login-button">Ingresar</button>
