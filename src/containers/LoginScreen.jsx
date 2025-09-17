693f9fc35c39f9d1c42450bd72e1a498916bf399
Lucas Sabino <sabino.lucas2016@gmail.com>
2025-09-17 00:53:58 -0300

    ING-7 Estructura general de la UI

--- src/containers/LoginScreen.jsx
+++ src/containers/LoginScreen.jsx
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginScreen.css';
 
+
 function LoginScreen() {
+
+  const [nombre, setNombre] = useState('');
+
+  const [fechaNacimiento, setFechaNacimiento] = useState('');
+  
+  // Ejecuta cuando se mandan los datos
+  function handleSubmit(event){
+    event.preventDefault(); // Previene el comportamiento por defecto de los inputs
+
+    console.log(nombre);
+    console.log(fechaNacimiento);
+  }
+
+  function handleNombre(event) {
+    setNombre(event.target.value);
+  }
+
+  function handleFechaNacimiento(event) {
+    setFechaNacimiento(event.target.value);
+  }
+
   return (
-    <div className="login-container" style={{ backgroundImage: `url('/main-screen-bg.jpg')` }}>
-      <div className="login-form">
-        <h1>Bienvenido al Juego</h1>
-        <input type="text" placeholder="Ingresa tu nombre" />
-        <input type="date" placeholder="Fecha de nacimiento" />
-        <button className="login-button">Ingresar</button>
+    <div className="screen-container">
+      <div className="input-container">
+        <h1>Ingresa tus datos</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="nombre">Nombre: </label>
+            <input 
+              type="text" 
+              id="nombre" 
+              value={nombre}
+              onChange={handleNombre}
+              required
+              placeholder="Ingresar nombre" />
+          </div>
+          <div className="input-goup">
+            <label htmlFor="fechaNacimiento">Fecha de nacimiento: </label>
+            <input 
+              type="date"
+              id="fechaNacimiento"
+              value={fechaNacimiento}
+              onChange={handleFechaNacimiento}
+              required />
+          </div>
+
+          <button type="submit" className="submit-btn">
+            Ingresar
+          </button>
+        </form>
       </div>
     </div>
   );
