4056850937215e8e83168b449ebb9abaf326f666
santti29 <santiago.sosa.alvarez@mi.unc.edu.ar>
2025-09-20 20:17:52 -0300

    ING-9 add number of players

---
+++ src/components/CantidadDeJugadores.jsx
@@ -0,0 +1,20 @@
+export default function CantidadDeJugadores({ jugadores, setJugadores }) {
+  const opciones = [2, 3, 4, 5, 6];
+
+  return (
+    <div>
+      <label>Cantidad de jugadores:</label>
+      <div className="cantidad-jugadores-container">
+        {opciones.map((num) => (
+          <button
+            key={num}
+            onClick={() => setJugadores(num)}
+            className={`btn-cantidad-jugador${jugadores === num ? " btn-cantidad-jugador-activo" : ""}`}
+            >
+            {num}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/containers/PantallaDeCreacion.jsx
+++ src/containers/PantallaDeCreacion.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import NombreDePartida from "../components/NombreDePartida";
+import CantidadDeJugadores from "../components/CantidadDeJugadores";
 
 export default function PantallaDeCreacion() {
   const [nombre, setNombre] = useState("");
-
+  const [jugadores, setJugadores] = useState(null);
+  
   return (
     <div className="pantalla-creacion">
       <div className="form-container">
         <NombreDePartida nombre={nombre} setNombre={setNombre} />
+        <CantidadDeJugadores jugadores={jugadores} setJugadores={setJugadores} />
       </div>
     </div>
   );
