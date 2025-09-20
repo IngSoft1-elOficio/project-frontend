ed72b977fd24dcfc726461b7f9324863c9062cd4
mariajosearandaviana <maria.aranda.viana@mi.unc.edu.ar>
2025-09-20 10:48:23 -0300

    ING-14 add GameJoin route

---
+++ src/containers/GameJoin.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+//import { useParams, useNavigate } from 'react-router-dom';
+
+function GameJoin(){
+    //const {id} = useParams();
+
+    return (
+        <h1>GameJoin</h1>
+
+    );
+    
+}
+
+export default GameJoin;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import GameJoin from './containers/GameJoin.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path ="/" element ={<App />}></Route>
+        <Route path = "/game_join/123" element = {<GameJoin/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
